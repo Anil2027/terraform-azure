2c8b37a679ed89803e43cd9dce9793dd95302c38
Anil2027 <125707973+Anil2027@users.noreply.github.com>
2023-05-23 09:45:36 +0530

    Add files via upload

---
+++ Creating an Activity Log alert via Terraform/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.55.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "add your account details"
+  tenant_id = "add your account details"
+  client_id = "add your account details"
+  client_secret = "add your account details"
+  features {}  
+}
